ed8c2f3c56314432312e84dd353822539ff9bfca
fob1493 <fob1493@gmail.com>
2018-04-24 10:38:11 +0100

    create todo

--- myproject/todoapi/views.py
+++ myproject/todoapi/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import TodoSerializer
+from .models import Todo
 
 # Create your views here.
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+    def perform_create(self, serializer):
+        """save the post data when creating a new bucketlist."""
+        serializer.save()
