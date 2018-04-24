828a30381e7b1454ac3ccdf89dcbccb0a92d76bd
fob1493 <fob1493@gmail.com>
2018-04-24 10:38:11 +0100

    create todo

--- myproject/todoapi/tests.py
+++ myproject/todoapi/tests.py
@@ -37,3 +37,35 @@ class ViewTestCase(TestCase):
     def test_api_can_create_a_todo(self):
         """Test the api has todo creation capability"""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_bucketlist(self):
+        """Test the api can get a given todo"""
+        todo = Todo.objects.get(id="1")
+        response = self.client.get(
+            reverse('details',
+            kwargs={'pk': todo.id}), format="json"
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, todo)
+
+    def test_api_can_update_todo(self):
+        """Test the api can update a given todo"""
+        change_todo = {'name': 'Something different'}
+        res = self.client.put(
+            reverse('details', kwargs={'pk': todo.id}),
+            change_todo,
+            format="json"
+        )
+        self.assertEqual(res.status_coee, status.HTTP_200_OK)
+
+    def test_api_can_delete_todo(self):
+        """Test the api can delete a todo"""
+        todo = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': todo.id}),
+            format='json',
+            follow=True
+        )
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
