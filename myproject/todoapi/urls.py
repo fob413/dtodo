2b0e751399f96a9e37f00faab2dd51eb359a7ac3
fob1493 <fob1493@gmail.com>
2018-04-26 12:15:56 +0100

    refactor code

--- myproject/todoapi/urls.py
+++ myproject/todoapi/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView, DetailsView
+from .views import todo_list, todo_detail
 
 urlpatterns = {
-    url(r'^todo/$', CreateView.as_view(), name="create"),
-    url(r'^todo/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
+    url(r'^todo/$', todo_list, name="create"),
+    url(r'^todo/(?P<pk>[0-9]+)/$', todo_detail, name="details"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
